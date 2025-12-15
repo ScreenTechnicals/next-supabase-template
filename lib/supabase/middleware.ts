d726d7e74f31bf06a5289ff7673c66576c3b4eab
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2025-12-15 13:33:07 +0530

    fix: client creation

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,35 @@
+import { createServerClient } from '@supabase/ssr';
+import { NextRequest, NextResponse } from 'next/server';
+import { SUPABASE_KEY, SUPABASE_URL } from './client';
+
+export async function updateSession(request: NextRequest) {
+    let supabaseResponse = NextResponse.next({
+        request,
+    });
+
+    const supabase = createServerClient(
+        SUPABASE_URL,
+        SUPABASE_KEY,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll();
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    });
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options)
+                    );
+                },
+            },
+        }
+    );
+
+    // refreshing the auth token
+    await supabase.auth.getUser();
+
+    return supabaseResponse;
+}
